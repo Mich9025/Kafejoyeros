43828dc5b877373e9b1bf8f4bf57aa8b340289d2
Mich9025 <iglobal.comercial@gmail.com>
2026-01-13 15:47:53 -0500

    Update layout.tsx

--- kafejoyeros/src/app/layout.tsx
+++ kafejoyeros/src/app/layout.tsx
@@ -21,6 +21,9 @@ const dmSans = DM_Sans({
 });
 
 export const metadata: Metadata = {
+  verification: {
+    google: "bPkOhsU3OZnnvwp7BELLugS_K4uuvr2yBh0XOQPgJ_A",
+  },
   title: "Kafe Joyeros - Joyería Artesanal de Excelencia",
   description: "Creamos joyas únicas que cuentan tu historia. Más de 20 años de experiencia en joyería artesanal, diseño personalizado, reparación y servicios especializados.",
   keywords: "joyería, joyas artesanales, diseño personalizado, anillos, collares, aretes, pulseras, oro, plata, diamantes, reparación de joyas",
